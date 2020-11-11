cd37f1f51ef0e4c648ab9f19cd3ee9c3bed14cfb
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-11-10 19:18:33 -0800

    winner

---
+++ 2gamestages/player.js
@@ -0,0 +1,51 @@
+
+class Player{
+	construtor(){
+		this.r = 60;
+		this.x = w / 2;
+		this.y = h - this.r;
+		this.speed = 2;
+		this.direction = 'still';
+
+	}
+
+	display(){
+		//rec(this.x, this.y, this.r, this.r)
+		image(playerImg, this.x, this.y, this.r, this.r)
+
+	}
+
+	move(){
+		switch (this.direction){
+			case 'still':
+			//dont move anything
+			break;
+			case 'up':
+			//decrease y pos
+			if (this.y > 0){
+			this.y -= this.speed;
+			}
+			break;
+			case 'down':
+			//increase y pos
+			if (this.y < h - this.r){
+			this.y += this.speed;
+			}
+			break;
+			case 'right':
+			//increase x pos
+			if (this.x < w - this.r){
+			this.x += this.speed;
+			}
+			break;
+			case 'left':
+			//decrease x pos
+			if (this.x > 0){
+			this.x -= this.speed;
+			}
+			break;
+			default:
+			break;
+			}
+	}
+}
