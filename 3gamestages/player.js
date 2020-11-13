20ede7e64b8d01a22d4488c5f71fb01e22c7785e
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-11-12 18:00:00 -0800

    ahhhhh

--- 3gamestages/player.js
+++ 3gamestages/player.js
@@ -1,7 +1,7 @@
 
 class Player{
 	constructor(){
-		this.r = 90;
+		this.r = 120;
 		this.x = w / 2;
 		this.y = h - this.r;
 		this.speed = 2;
--- 3gamestages/sketch.js
+++ 3gamestages/sketch.js
@@ -86,7 +86,7 @@ function title(){
   text('Collect Pearls',w/2, h/5);
   textSize(40);
   text('click anywhere to start',w/2, h/2);
-  image(witchImg, w / 2, h - 90, 90, 90)
+  image(witchImg, w / 2, h - 120, 120, 120)
 }
 
 function titleMouseClicked(){
