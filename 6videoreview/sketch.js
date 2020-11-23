438c25e2082f0215b6315c22d631582b4e7235fe
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-11-23 10:37:24 -0800

    ahcancnaocava notes

--- 6videoreview/sketch.js
+++ 6videoreview/sketch.js
@@ -13,7 +13,11 @@
 //x = x-3 and x=x+3 is flawed because it only moves back once
 //you need to make it stop looping with varaible speed
 //have draw loops do something different based on true and false
-
+//var are all numbers but there is set ones like mouseX or ones we set
+//mouseIsPressed is a a bolean or t/f
+//funcion mousePressed is an event or when action is deployed
+//theres a differnece from mouse click and mouse click/held
+//
 
 var x = 0;
 var speed = 3;
@@ -89,8 +93,9 @@ function draw() {
   //   point(300, 200);
   // }
 
-  if (mouseX > 300 && mouseX < 400) {
+  if (mouseX > 250 && mouseX < 350 &&  mouseY > 150 && mouseY < 250){
     fill(255, 0, 200);
   }
+  rectMode(CENTER);
   rect(300, 200, 100, 100);
 }
