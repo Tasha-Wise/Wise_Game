11f0d4719a9fce2ea7f1077ee4c1b47e27de466b
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-12-01 17:02:02 -0800

    pushhh

--- 7gamestages/player.js
+++ 7gamestages/player.js
@@ -23,25 +23,25 @@ class Player{
 			break;
 			case 'up':
 			//decrease y pos
-			if (this.y - this.r/2 > 0){
+			if (this.y > 0){
 			this.y -= this.speed;
 			}
 			break;
 			case 'down':
 			//increase y pos
-			if (this.y < h - this.r/2){
+			if (this.y < h - this.r){
 			this.y += this.speed;
 			}
 			break;
 			case 'right':
 			//increase x pos
-			if (this.x < w - this.r/2){
+			if (this.x < w - this.r){
 			this.x += this.speed;
 			}
 			break;
 			case 'left':
 			//decrease x pos
-			if (this.x - this.r/2> 0){
+			if (this.x > 0){
 			this.x -= this.speed;
 			}
 			break;
--- 7gamestages/witch.js
+++ 7gamestages/witch.js
@@ -1,7 +1,6 @@
-
-class Player{
+class Witch{
 	constructor(){
-		this.r = 100;
+		this.r = 90;
 		this.x = w / 2;
 		this.y = h - this.r;
 		this.speed = 2;
@@ -11,7 +10,7 @@ class Player{
 
 	display(){
 		//rec(this.x, this.y, this.r, this.r)
-		image(playerImg, this.x, this.y, this.r, this.r)
+		image(witchImg, this.x, this.y, this.r, this.r)
 
 	}
 
@@ -23,25 +22,25 @@ class Player{
 			break;
 			case 'up':
 			//decrease y pos
-			if (this.y - this.r/2 > 0){
+			if (this.y > 0){
 			this.y -= this.speed;
 			}
 			break;
 			case 'down':
 			//increase y pos
-			if (this.y < h - this.r/2){
+			if (this.y < h - this.r){
 			this.y += this.speed;
 			}
 			break;
 			case 'right':
 			//increase x pos
-			if (this.x < w - this.r/2){
+			if (this.x < w - this.r){
 			this.x += this.speed;
 			}
 			break;
 			case 'left':
 			//decrease x pos
-			if (this.x - this.r/2> 0){
+			if (this.x > 0){
 			this.x -= this.speed;
 			}
 			break;
