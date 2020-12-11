de620d123232aa7468d361bee9d0ac99763e64e4
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-12-11 12:26:44 -0800

    no animation

--- ScarlettGame/player.js
+++ ScarlettGame/player.js
@@ -10,8 +10,8 @@ class Player{
 
 	display(){
 		//rec(this.x, this.y, this.r, this.r);
-		//image(playerImg, this.x, this.y, this.r, this.r);
-		image(playerAnimation[frameCount % playerAnimation.length], this.x, this.y, this.r, this.r);
+		image(playerImg, this.x, this.y, this.r, this.r);
+		//image(playerAnimation[frameCount % playerAnimation.length], this.x, this.y, this.r, this.r);
 
 	}
 
